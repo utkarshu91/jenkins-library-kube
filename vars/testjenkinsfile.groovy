8b4722399e9b77677f4b04b8733f51f08c71d74f
utkarsh upadhyay <utkarshu91@gmail.com>
2019-06-04 16:37:58 +0530

    Update testjenkinsfile.groovy

--- vars/testjenkinsfile.groovy
+++ vars/testjenkinsfile.groovy
@@ -1,61 +1,65 @@
-pipeline {
-    agent {
-        any {
-            label "uiui"
-            defaultContainer 'jnlp'
-            yamlFile '''
-apiVersion: v1
-kind: Pod
-metadata:
-labels:
-component: ci
-spec:
-  # Use service account that can deploy to all namespaces
- # cloud: kubernetes
-  serviceAccountName: default
-  containers:
-  - name: maven
-    image: gcr.io/cloud-builders/mvn:3.5.0-jdk-8
-    command:
-    - cat
-    tty: true
-    volumeMounts:
-    - name: agent-data
-      mountPath: /root/
-  - name: git
-    image: gcr.io/cloud-builders/git 
-    command:
-    - cat
-    tty: true
-    volumeMounts:
-    - name: agent-data
-      mountPath: /root/
-  restartPolicy: Never
-  volumes:
-    - name: agent-data
-      emptyDir: {}  
-      '''
-            }
-        }
-    stages {
-        stage("checkout"){
-            steps {
-                container('git'){
-                    sh "ls -l"
-                    }
-                
-                }
+#!/usr/bin/env groovy
 
-            }
-        stage("Maven Build") {
-            steps {
-                container('maven') {
-                   
-                       sh "ls -l"
-                     
-                    }
+def call(Map param) {
+    pipeline {
+        agent {
+            any {
+                label "uiui"
+                defaultContainer 'jnlp'
+                yamlFile '''
+    apiVersion: v1
+    kind: Pod
+    metadata:
+    labels:
+    component: ci
+    spec:
+    # Use service account that can deploy to all namespaces
+    # cloud: kubernetes
+    serviceAccountName: default
+    containers:
+    - name: maven
+        image: gcr.io/cloud-builders/mvn:3.5.0-jdk-8
+        command:
+        - cat
+        tty: true
+        volumeMounts:
+        - name: agent-data
+        mountPath: /root/
+    - name: git
+        image: gcr.io/cloud-builders/git 
+        command:
+        - cat
+        tty: true
+        volumeMounts:
+        - name: agent-data
+        mountPath: /root/
+    restartPolicy: Never
+    volumes:
+        - name: agent-data
+        emptyDir: {}  
+        '''
                 }
-            }   
+            }
+        stages {
+            stage("checkout"){
+                steps {
+                    container('git'){
+                        sh "ls -l"
+                        }
+                    
+                    }
+
+                }
+            stage("Maven Build") {
+                steps {
+                    container('maven') {
+                    
+                        sh "ls -l"
+                        
+                        }
+                    }
+                }   
+        }
+        
     }
-    
 }
