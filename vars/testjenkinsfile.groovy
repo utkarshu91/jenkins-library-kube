13814862f168ba42eedacdeefa2c8f53b6c3da2c
utkarsh upadhyay <utkarshu91@gmail.com>
2019-06-04 17:03:10 +0530

    Update testjenkinsfile.groovy

--- vars/testjenkinsfile.groovy
+++ vars/testjenkinsfile.groovy
@@ -1,43 +1,13 @@
 #!/usr/bin/env groovy
 
 def call(Map param) {
+    def containerAgent = libraryResource 'kubernetespod.yaml'
     pipeline {
         agent {
             any {
                 label "uiui"
                 defaultContainer 'jnlp'
-                yamlFile '''
-    apiVersion: v1
-    kind: Pod
-    metadata:
-    labels:
-    component: ci
-    spec:
-    # Use service account that can deploy to all namespaces
-    # cloud: kubernetes
-    serviceAccountName: default
-    containers:
-    - name: maven
-        image: gcr.io/cloud-builders/mvn:3.5.0-jdk-8
-        command:
-        - cat
-        tty: true
-        volumeMounts:
-        - name: agent-data
-        mountPath: /root/
-    - name: git
-        image: gcr.io/cloud-builders/git 
-        command:
-        - cat
-        tty: true
-        volumeMounts:
-        - name: agent-data
-        mountPath: /root/
-    restartPolicy: Never
-    volumes:
-        - name: agent-data
-        emptyDir: {}  
-        '''
+                yamlFile "$containerAgent"
                 }
             }
         stages {
