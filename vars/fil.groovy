29393601e6cbcea9559feba60dc386042fb0bc0e
utkarsh upadhyay <utkarshu91@gmail.com>
2019-06-07 11:57:22 +0530

    Create fil.groovy

---
+++ vars/fil.groovy
@@ -0,0 +1,44 @@
+#!/usr/bin/env groovy
+
+def call(Map param) {
+    def appName = param.appName
+    def containerAgent = libraryResource 'kubernetespod.yaml'
+    def GCR_DNS = "asia.gcr.io"
+    def GCR_PROJECT_ID = "nonprod-utility-233414"
+    def GCR_APP_PATH_PREFIX = param.appName
+    def SERVICE_TYPE = param.service_type
+    pipeline {
+        agent {
+            any {
+                label "$appName"
+                defaultContainer 'jnlp'
+                yamlFile "$containerAgent"
+                }
+            }
+        stages {
+            stage("checkout"){
+                steps {
+                    container('git'){
+                        sh "ls -l"
+                        sh "echo $appName"
+                        sh "echo $GCR_PROJECT_ID"
+                        sh "echo $GCR_APP_PATH_PREFIX"
+                        sh "echo $SERVICE_TYPE"
+                        }
+                    
+                    }
+
+                }
+            stage("Maven Build") {
+                steps {
+                    container('maven') {
+                    
+                        sh "ls -l"
+                        
+                        }
+                    }
+                }   
+        }
+        
+    }
+}
