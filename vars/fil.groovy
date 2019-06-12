b4899888588afc5f7410adc61da7a05e7386f0d2
utkarsh upadhyay <utkarshu91@gmail.com>
2019-06-12 11:26:41 +0530

    Update fil.groovy

--- vars/fil.groovy
+++ vars/fil.groovy
@@ -2,6 +2,7 @@
 
 def call(Map param) {
     def appName = param.appName
+    def TESTVAR = param.TESTVAR
     def containerAgent = libraryResource 'agent.yaml'
     def GCR_DNS = "asia.gcr.io"
     def GCR_PROJECT_ID = "nonprod-utility-233414"
@@ -11,7 +12,40 @@ def call(Map param) {
         agent {
             any {
                 label "$appName"
-                yaml libraryResource('agent.yaml')
+                yaml libraryResource '''
+apiVersion: v1
+kind: Pod
+metadata:
+labels:
+component: ci
+spec:
+  # this is agent.yaml file
+  # Use service account that can deploy to all namespaces
+ # cloud: kubernetes
+  serviceAccountName: default
+  containers:
+  - name: maven
+    image: gcr.io/cloud-builders/mvn:3.5.0-jdk-8
+    command:
+    - cat
+    tty: true
+    volumeMounts:
+    - name: agent-data
+      mountPath: /root/
+  - name: git
+    image: gcr.io/cloud-builders/git 
+    command:
+    - cat
+    tty: true
+    volumeMounts:
+    - name: agent-data
+      mountPath: /root/
+  restartPolicy: Never
+  volumes:
+    - name: agent-data
+      emptyDir: {}
+'''
+
                 defaultContainer 'jnlp'
                 }
             }
@@ -21,6 +55,7 @@ def call(Map param) {
                     container('git'){
                         sh "ls -l"
                         sh "echo $appName"
+                        sh "echo $TESTVAR"
                         sh "echo $GCR_PROJECT_ID"
                         sh "echo $GCR_APP_PATH_PREFIX"
                         sh "echo $SERVICE_TYPE"
